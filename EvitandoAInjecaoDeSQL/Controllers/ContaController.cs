1f52255b21230e7c04504a878e97258637d227b6
fabiovigon <fabiovigon11@gmail.com>
2021-09-19 16:25:01 -0300

    Estudo do sql injection

---
+++ EvitandoAInjecaoDeSQL/Controllers/ContaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data.SqlClient;
+
+namespace EvitandoAInjecaoDeSQL.Controllers
+{
+    public class ContaController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string usuario, string senha)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=VendasDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var consulta = "SELECT COUNT(*) FROM Usuario WHERE Usuario = '" + usuario + "' AND Senha = '" + senha + "'";
+
+            try
+            {
+                using (var conexao = new SqlConnection(connectionString))
+                {
+                    conexao.Open();
+
+                    using (SqlCommand comando = new SqlCommand(consulta, conexao))
+                    {
+                        var resultado = (int)comando.ExecuteScalar();
+                        if (resultado > 0)
+                            ViewBag.Mensagem = "Login efetuado com sucesso";
+                        else
+                            ViewBag.Mensagem = "Falha no login";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Mensagem = "Erro: " + e.Message;
+            }
+
+            return View();
+        }
+    }
+}
